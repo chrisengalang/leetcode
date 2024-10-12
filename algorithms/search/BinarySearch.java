f6ab9578af5ad4dc0d34e1b77a011ae2a7f20ad7
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-10-12 11:15:53 -0700

    Add recursive binary search.

--- algorithms/search/BinarySearch.java
+++ algorithms/search/BinarySearch.java
@@ -30,10 +30,51 @@ public class BinarySearch {
     return -1;
   }
 
+  /**
+   * Recursive form of binary search.
+   *
+   * Time complexity: O(log n) The window of the search is divided in half with each recursive call. Same as iterative.
+   * Space complexity: O(log n) A call is added the call stack with each layer of recursive call.
+   *
+   * @param numbers
+   * @param target
+   * @return
+   */
+  public static int recursiveBinarySearch(int[] numbers, int target) {
+    return recursiveBinarySearch(numbers, target, 0, numbers.length - 1);
+  }
+
+  /**
+   * Helper function for the recursive binary search. Add the parameters of the start and end of the search window.
+   *
+   * @param numbers
+   * @param target
+   * @param left
+   * @param right
+   * @return
+   */
+  private static int recursiveBinarySearch(int[] numbers, int target, int left, int right) {
+    if (left > right) {
+      return -1;
+    }
+
+    int half = left + (right - left) / 2;
+
+    if (target == numbers[half]) {
+      return half;
+    }
+
+    if (target < numbers[half]) {
+      return recursiveBinarySearch(numbers, target, left, half - 1);
+    } else {
+      return recursiveBinarySearch(numbers, target, half + 1, right);
+    }
+  }
 
   public static void main(String[] args) {
     int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    System.out.println("The number is at index: " + binarySearch(numbers, 1));
+    System.out.println("The number is at index: " + binarySearch(numbers, 0));
+    System.out.println("The number is at index: " + recursiveBinarySearch(numbers, 0));
   }
 }
